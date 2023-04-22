7fc5cda610f69c11b87459de69edb683fbcc17f8
c0desbym3ta <m3tahckr@protonmail.com>
2023-04-22 14:22:10 -0400

    Add txt file as parameter

--- email_hunter_multitool.sh
+++ email_hunter_multitool.sh
@@ -3,7 +3,7 @@
 # This is a super simple way to check for email after a given domain. Here's how it works:
 #
 #     - Mail the script executable: chmod +x email_hunter_multitool.sh
-#     - Run the script ./email_hunter_multitool.sh domain.com
+#     - Run the script ./email_hunter_multitool.sh domain.com emails_found.txt
 #     - Enter the path to save results
 #
 # This script uses a set of tools to search for mails given a certain domain and then saves the ouput to a txt file.
@@ -35,7 +35,7 @@
 
 ## First and only argument
 domain_name=$1
-
+file_name=$2
 
 ## Setting up colors
 RED="\e[31m"
@@ -44,22 +44,21 @@ YELLOW="\e[33m"
 ENDCOLOR="\e[0m"
 
 ## Empty file variable
-new_file="emails_found.txt"
 breached_credentials="breached_credentials.txt"
 
 ## Create empty file to save all emails
-touch $new_file
-cat "" > $new_file
+touch $2
+echo "" > $2
 
 ## Fire up theHarvester
 echo -e "${YELLOW}[\] Searching emails using theHarverster...${ENDCOLOR}"
-## theHarvester -d $domain_name -b all | grep "@" > $new_file
-echo -e "${GREEN}[+] theHarvester done... Emails saved on emails_found.txt${ENDCOLOR}"
+## theHarvester -d $domain_name -b all | grep "@" > $2
+echo -e "${GREEN}[+] theHarvester done... Emails saved on $2${ENDCOLOR}"
 
 ## Fire up emailHarvester
 echo -e "${YELLOW}[\] Searching emails using emailHarverster...${ENDCOLOR}"
-## emailharvester -d $domain_name -l 10 | grep "@" >> $new_file
-echo -e "${GREEN}[+] emailHarvester done... Emails saved on emails_found.txt${ENDCOLOR}"
+## emailharvester -d $domain_name -l 10 | grep "@" >> $2
+echo -e "${GREEN}[+] emailHarvester done... Emails saved on $2${ENDCOLOR}"
 
 ## Curling from skymem website
 startpage=1
@@ -72,16 +71,16 @@ company_name=${domain%%.*}
 for (( i=$startpage; i<$lastpage; i++))
 do
     emails=$(curl -s http://www.skymem.info/domain/$skymem$i | grep "$company_name" | grep '<a href="/srch?q=' | sed '1d' | sed -e 's/<[^>]*>//g' | sed -e 's/^[ \t]*//')
-    echo "$emails" >> $new_file
+    echo "$emails" >> $2
 done
-echo -e "${GREEN}[+] Skymem.info done... Emails saved on emails_found.txt${ENDCOLOR}"
+echo -e "${GREEN}[+] Skymem.info done... Emails saved on $2${ENDCOLOR}"
 
-result=$(cat $new_file | wc -l)
+result=$(cat $2 | wc -l)
 echo -e "${GREEN}[+] ${result} emails has been found${ENDCOLOR}"
 
 read -p "Do you want to view the emails found?: (y/n): " view
 if [[ $view == "y" || $view == "Y" || $view == "yes" ]]; then
-    cat $new_file
+    cat $2
     echo " "
 fi
 
@@ -89,9 +88,9 @@ fi
 read -p "Do you want to use the emails found for breached credentials? (y/n): " choice
 if [[ $choice == "y" || $choice == "Y" || $choice == "yes" ]]; then
 
-	echo -e "${YELLOW}[\] Searching emails from emails_found.txt for breached credentialas...${ENDCOLOR}"
+	echo -e "${YELLOW}[\] Searching emails from $2 for breached credentialas...${ENDCOLOR}"
 	echo -e "${YELLOW}[\] Searching for beached credentials using h8mail...${ENDCOLOR}"
-	h8mail -t $new_file > $breached_credentials
+	h8mail -t $2 > $breached_credentials
 	echo -e "${GREEN}[+] h8mail is done... Results saved on breached_credentials.txt"
 
 	read -p "Do you want to view breached emails found? (y/n): "  view
